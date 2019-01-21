73b82958b8cfdbe1383afa4ac8a0de0981e22185
Garrian BrownE <garrianbrown@gmail.com>
2019-01-21 14:08:38 -0600

    Added files for existing todo list app.

---
+++ mindList/app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+	def new
+		@task = Task.new
+		render :new
+	end
+
+	def create
+
+	end
+
+	def edit
+		
+	end
+
+	def destroy
+		
+	end
+
+	def save
+		
+	end
+end
